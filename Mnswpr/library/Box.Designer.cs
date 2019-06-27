7657351b5fbcad5aee1b932df893eff9e4f087e4
unknown <dhmm@dhmm>
2019-06-27 16:33:56 +0300

    mouse buttons added

--- Mnswpr/library/Box.Designer.cs
+++ Mnswpr/library/Box.Designer.cs
@@ -28,20 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Box));
+            this.pctFlag = new System.Windows.Forms.PictureBox();
+            this.lblMines = new System.Windows.Forms.Label();
+            this.btnBox = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFlag)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pctFlag
+            // 
+            this.pctFlag.Image = ((System.Drawing.Image)(resources.GetObject("pctFlag.Image")));
+            this.pctFlag.Location = new System.Drawing.Point(0, 0);
+            this.pctFlag.Name = "pctFlag";
+            this.pctFlag.Size = new System.Drawing.Size(20, 20);
+            this.pctFlag.TabIndex = 0;
+            this.pctFlag.TabStop = false;
+            this.pctFlag.Visible = false;
+            // 
+            // lblMines
+            // 
+            this.lblMines.Location = new System.Drawing.Point(0, 0);
+            this.lblMines.Name = "lblMines";
+            this.lblMines.Size = new System.Drawing.Size(20, 20);
+            this.lblMines.TabIndex = 1;
+            this.lblMines.Visible = false;
+            // 
+            // btnBox
+            // 
+            this.btnBox.BackColor = System.Drawing.SystemColors.Control;
+            this.btnBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBox.Location = new System.Drawing.Point(0, 0);
+            this.btnBox.Margin = new System.Windows.Forms.Padding(0);
+            this.btnBox.Name = "btnBox";
+            this.btnBox.Size = new System.Drawing.Size(20, 20);
+            this.btnBox.TabIndex = 2;
+            this.btnBox.UseVisualStyleBackColor = false;
+            this.btnBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnBox_MouseDown);
+            this.btnBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnBox_MouseUp);
             // 
             // Box
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btnBox);
+            this.Controls.Add(this.pctFlag);
+            this.Controls.Add(this.lblMines);
+            this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "Box";
-            this.Size = new System.Drawing.Size(18, 18);
+            this.Size = new System.Drawing.Size(20, 20);
+            ((System.ComponentModel.ISupportInitialize)(this.pctFlag)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pctFlag;
+        private System.Windows.Forms.Label lblMines;
+        private System.Windows.Forms.Button btnBox;
     }
 }
--- Mnswpr/library/Box.cs
+++ Mnswpr/library/Box.cs
@@ -12,9 +12,62 @@ namespace Mnswpr.library
     public partial class Box : UserControl
     {
         public bool HasMine { set; get; } = false;
+        public bool HasFlag
+        {
+            set
+            {
+                if(value)
+                {
+                    pctFlag.Show();
+                }
+                else
+                {
+                    pctFlag.Hide();
+                }
+            }
+            get
+            {
+                return pctFlag.Visible;
+            }
+        } 
         public Box()
         {
             InitializeComponent();
         }
+
+        bool RightButton = false;
+        bool LeftButton = false;        
+        private void btnBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                RightButton = true;
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                LeftButton = true;
+            }
+        }
+
+        private void btnBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && !RightButton)
+            {
+                MessageBox.Show("L");
+            }
+            else if (e.Button == MouseButtons.Right && !LeftButton)
+            {
+                MessageBox.Show("R");
+            }
+            else if (e.Button == MouseButtons.Right && LeftButton)
+            {
+                MessageBox.Show("L + R");
+            }
+
+            RightButton = false;
+            LeftButton = false;
+        }
+
     }
 }
